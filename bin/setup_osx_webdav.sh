2e6635c7a981e52fe2bdc520c3bb1be2cfe03599
Dmitriy Shabanov <shabanovd@gmail.com>
2016-05-04 22:42:59 +0300

    root is 3 levels up

--- bin/setup_osx_webdav.sh
+++ bin/setup_osx_webdav.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ../..
+cd ../../..
 
 cd "Oxygen XML Author.app/Contents/"
 plutil -replace CFBundleURLTypes -xml '<dict><key>CFBundleTypeRole</key><string>Viewer</string><key>CFBundleURLName</key><string>Webdav URL</string><key>CFBundleURLSchemes</key><array><string>webdav</string><string>webdavs</string></array></dict>' Info.plist
