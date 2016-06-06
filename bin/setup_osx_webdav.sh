bbf3d7cd02f2802678b277b868aaca1e453140a2
Dmitriy Shabanov <shabanovd@gmail.com>
2016-06-06 21:13:25 +0300

    add missing "<array>"

--- bin/setup_osx_webdav.sh
+++ bin/setup_osx_webdav.sh
@@ -3,7 +3,7 @@
 cd ../../..
 
 cd "Oxygen XML Author.app/Contents/"
-plutil -replace CFBundleURLTypes -xml '<dict><key>CFBundleTypeRole</key><string>Viewer</string><key>CFBundleURLName</key><string>Webdav URL</string><key>CFBundleURLSchemes</key><array><string>webdav</string><string>webdavs</string></array></dict>' Info.plist
+plutil -replace CFBundleURLTypes -xml '<array><dict><key>CFBundleTypeRole</key><string>Viewer</string><key>CFBundleURLName</key><string>Webdav URL</string><key>CFBundleURLSchemes</key><array><string>webdav</string><string>webdavs</string></array></dict></array>' Info.plist
 
 cd "../../Oxygen XML Editor.app/Contents/"
-plutil -replace CFBundleURLTypes -xml '<dict><key>CFBundleTypeRole</key><string>Viewer</string><key>CFBundleURLName</key><string>Webdav URL</string><key>CFBundleURLSchemes</key><array><string>webdav</string><string>webdavs</string></array></dict>' Info.plist
+plutil -replace CFBundleURLTypes -xml '<array><dict><key>CFBundleTypeRole</key><string>Viewer</string><key>CFBundleURLName</key><string>Webdav URL</string><key>CFBundleURLSchemes</key><array><string>webdav</string><string>webdavs</string></array></dict></array>' Info.plist
