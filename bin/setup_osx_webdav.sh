a67e8ddf6b487ae2c9a22eb7c17fe86770b6111c
Dmitriy Shabanov <shabanovd@gmail.com>
2016-10-29 18:23:43 +0300

    make copy of "Oxygen XML Author.app" and "Oxygen XML Editor.app"

--- bin/setup_osx_webdav.sh
+++ bin/setup_osx_webdav.sh
@@ -2,6 +2,10 @@
 
 cd ../../..
 
+#make copy
+cp -nR "Oxygen XML Author.app/" "Oxygen XML Author backup.app/"
+cp -nR "Oxygen XML Editor.app/" "Oxygen XML Editor backup.app/"
+
 cd "Oxygen XML Author.app/Contents/"
 plutil -replace CFBundleURLTypes -xml '<array><dict><key>CFBundleTypeRole</key><string>Viewer</string><key>CFBundleURLName</key><string>Webdav URL</string><key>CFBundleURLSchemes</key><array><string>webdav</string><string>webdavs</string></array></dict></array>' Info.plist
 
