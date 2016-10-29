4c955049ac42ba2362f65749ee2bbd0a5aaab257
Joe Wicentowski <joewiz@gmail.com>
2016-10-29 01:03:00 -0400

    copy Info.plist to .bak file before changing it
    closes #3

--- bin/setup_osx_webdav.sh
+++ bin/setup_osx_webdav.sh
@@ -3,7 +3,9 @@
 cd ../../..
 
 cd "Oxygen XML Author.app/Contents/"
+cp Info.plist Info.plist.bak
 plutil -replace CFBundleURLTypes -xml '<array><dict><key>CFBundleTypeRole</key><string>Viewer</string><key>CFBundleURLName</key><string>Webdav URL</string><key>CFBundleURLSchemes</key><array><string>webdav</string><string>webdavs</string></array></dict></array>' Info.plist
 
 cd "../../Oxygen XML Editor.app/Contents/"
+cp Info.plist Info.plist.bak
 plutil -replace CFBundleURLTypes -xml '<array><dict><key>CFBundleTypeRole</key><string>Viewer</string><key>CFBundleURLName</key><string>Webdav URL</string><key>CFBundleURLSchemes</key><array><string>webdav</string><string>webdavs</string></array></dict></array>' Info.plist
