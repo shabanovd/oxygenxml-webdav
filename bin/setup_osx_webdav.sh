daa6c75be9ea0651f84bc672451f538ef0a7cb7c
Dmitriy Shabanov <shabanovd@gmail.com>
2016-12-15 19:11:03 +0300

    update setting only when application installed

--- bin/setup_osx_webdav.sh
+++ bin/setup_osx_webdav.sh
@@ -4,12 +4,38 @@ cd "$(dirname "$0")"
 
 cd ../../..
 
-#make copy
-cp -nR "Oxygen XML Author.app/" "Oxygen XML Author backup.app/"
-cp -nR "Oxygen XML Editor.app/" "Oxygen XML Editor backup.app/"
+if [ -d "Oxygen XML Author.app/" ]
+then
+    echo "updating 'Oxygen XML Author.app/'"
+    
+    #make copy
+    if [ ! -d "Oxygen XML Author backup.app/" ]
+    then
+        echo "make backup copy of 'Oxygen XML Author.app/'"
+        cp -nR "Oxygen XML Author.app/" "Oxygen XML Author backup.app/"
+    fi
+    
+    cd "Oxygen XML Author.app/Contents/"
+    plutil -replace CFBundleURLTypes -xml '<array><dict><key>CFBundleTypeRole</key><string>Viewer</string><key>CFBundleURLName</key><string>Webdav URL</string><key>CFBundleURLSchemes</key><array><string>webdav</string><string>webdavs</string></array></dict></array>' Info.plist
+    cd "../.."
+    
+    echo "'Oxygen XML Author.app' updated"
+fi
 
-cd "Oxygen XML Author.app/Contents/"
-plutil -replace CFBundleURLTypes -xml '<array><dict><key>CFBundleTypeRole</key><string>Viewer</string><key>CFBundleURLName</key><string>Webdav URL</string><key>CFBundleURLSchemes</key><array><string>webdav</string><string>webdavs</string></array></dict></array>' Info.plist
-
-cd "../../Oxygen XML Editor.app/Contents/"
-plutil -replace CFBundleURLTypes -xml '<array><dict><key>CFBundleTypeRole</key><string>Viewer</string><key>CFBundleURLName</key><string>Webdav URL</string><key>CFBundleURLSchemes</key><array><string>webdav</string><string>webdavs</string></array></dict></array>' Info.plist
+if [ -d "Oxygen XML Editor.app/" ]
+then
+    echo "updating 'Oxygen XML Editor.app'"
+    
+    #make copy
+    if [ ! -d "Oxygen XML Author backup.app/" ]
+    then
+        echo "make backup copy of 'Oxygen XML Editor.app/'"
+        cp -nR "Oxygen XML Editor.app/" "Oxygen XML Editor backup.app/"
+    fi
+    
+    cd "Oxygen XML Editor.app/Contents/"
+    plutil -replace CFBundleURLTypes -xml '<array><dict><key>CFBundleTypeRole</key><string>Viewer</string><key>CFBundleURLName</key><string>Webdav URL</string><key>CFBundleURLSchemes</key><array><string>webdav</string><string>webdavs</string></array></dict></array>' Info.plist
+    cd "../.."
+    
+    echo "'Oxygen XML Editor.app' updated"
+fi
